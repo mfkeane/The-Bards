db11ffcbb2c1bae40801b19f7885e2da52749811
courtneyd96 <37386976+courtneyd96@users.noreply.github.com>
2018-04-12 10:09:33 +1000

    Update Part B.py

--- Part B.py
+++ Part B.py
@@ -66,6 +66,63 @@ class Player:
             # Moving Phase
             # Will return a nested tuple
             
+                # Function to check the avaliable moves surrounding a piece
+    def check_moves(board, x, y):
+        moves = 0
+        # Check square to right
+        if (x+1 in range(8)) and (board[x+1][y] is EMPTY):
+            moves += 1
+        # Check square to left
+        if (x-1 in range(8)) and (board[x-1][y] is EMPTY):
+            moves += 1
+        # Check square below
+        if (y+1 in range(8)) and (board[x][y+1] is EMPTY):
+            moves += 1
+        # Check square above
+        if (y-1 in range(8)) and (board[x][y-1] is EMPTY):
+            moves += 1
+
+        # Check if piece can jump to right
+        if (x+2 in range(8)) and ((board[x+1][y] is WHITE) or
+           (board[x+1][y] is BLACK)) and (board[x+2][y] is EMPTY):
+            moves += 1
+        # Check if piece can jump to left
+        if (x-2 in range(8)) and ((board[x-1][y] is WHITE) or
+           (board[x-1][y] is BLACK)) and (board[x-2][y] is EMPTY):
+            moves += 1
+        # Check if piece can jump down
+        if (y+2 in range(8)) and ((board[x][y+1] is WHITE) or
+           (board[x][y+1] is BLACK)) and (board[x][y+2] is EMPTY):
+            moves += 1
+        # Check if piece can jump up
+        if (y-2 in range(8)) and ((board[x][y-1] is WHITE) or
+           (board[x][y-1] is BLACK)) and (board[x][y-2] is EMPTY):
+            moves += 1
+
+        return moves
+    
+        def moves(board):
+        # Initialise moves variables
+        moves_O = 0
+        moves_at = 0
+
+        # For each square on board:
+        #       - check if it's a piece
+        #       - if so, count avaliable moves
+
+        for x in range(8):
+            for y in range(8):
+                if board[x][y] is WHITE:
+                    # Check avaliable spaces
+                    moves_O += WatchYourBack.check_moves(board, x, y)
+
+                # Count available moves for BLACK
+                elif board[x][y] is BLACK:
+                    moves_at += WatchYourBack.check_moves(board, x, y)
+
+        print(str(moves_O) + "\n" + str(moves_at))
+        return [moves_O,  moves_at]
+            
             # move piece, update my_pos and board
     
         return action
